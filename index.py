407c655b33cdf09704a071fd594a1b50e9c3e641
Lucas-Guedes-Dev <lucas_guidolinguedes@outlookk.com>
2022-09-18 21:25:05 -0300

    Changes to be committed: 	new file:   __pycache__/connection_db.cpython-39.pyc 	modified:   index.py

--- index.py
+++ index.py
@@ -1,15 +1,16 @@
 from flask import Flask, request
+import json
+from connection_db import Querys
 
 app = Flask(__name__)
 
 @app.route('/insert/produto', methods=['POST'])
 def produto():
-    print('ola')
     if request.method == 'POST':
 
-        data = request.form
-        print(data)
+        data = request.get_json()
+        print(data['produto'])
         return data
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
